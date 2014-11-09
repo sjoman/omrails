f2369433249474b1608aede055e510dd1cb6a13c
sjoman <sjoman@gmail.com>
2014-11-09 23:30:29 +0100

    la till en route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'static_pages#home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
