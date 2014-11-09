cdec4803231477175bb3a73f2a44bd5aa05b7b9d
sjoman <sjoman@gmail.com>
2014-11-09 23:38:19 +0100

    random test2

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,16 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'static_pages#home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  initializer :add_builtin_route do |app|
+    if Rails.env.development?
+      app.routes.append do
+        get '/rails/info/properties' => "rails/info#properties"
+        get '/rails/info/routes'     => "rails/info#routes"
+        get '/rails/info'            => "rails/info#index"
+        get '/'                      => "rails/welcome#index"
+      end
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
